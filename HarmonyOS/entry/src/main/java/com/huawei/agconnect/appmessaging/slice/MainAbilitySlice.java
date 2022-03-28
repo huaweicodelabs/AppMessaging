35cf734ca1ae64c6215bf9d60031d98c59aabbd2
mayism <lican22@huawei.com>
2022-03-28 09:45:08 +0800

    harmonyos/add

---
+++ HarmonyOS/entry/src/main/java/com/huawei/agconnect/appmessaging/slice/MainAbilitySlice.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huawei.agconnect.appmessaging.slice;
+
+import com.huawei.agconnect.appmessaging.ResourceTable;
+import com.huawei.agconnect.appmessaging.AGConnectAppMessaging;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+import ohos.agp.components.Button;
+
+public class MainAbilitySlice extends AbilitySlice {
+
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_ability_main);
+
+        Button createShort = (Button) findComponentById(ResourceTable.Id_button_debug);
+        createShort.setClickedListener(component ->
+            debugMessage());
+    }
+
+    private void debugMessage() {
+        AGConnectAppMessaging.getInstance().setForceFetch();
+    }
+}
---
+++ HarmonyOS/entry/src/test/java/com/huawei/agconnect/appmessaging/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.huawei.agconnect.appmessaging;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
